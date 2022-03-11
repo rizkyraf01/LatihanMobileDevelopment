5c8029c7b36e8be80730327546be8a54384fd526
Andan Fathullah <fathullah.auzan@gmail.com>
2022-03-11 10:51:41 +0700

    Reupload

--- ContohRecycler/app/src/main/java/com/example/contohrecycler/MainActivity.kt
+++ ContohRecycler/app/src/main/java/com/example/contohrecycler/MainActivity.kt
@@ -13,10 +13,6 @@ import com.example.contohrecycler.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var listHeroAdapter: ListHeroAdapter
-    private lateinit var dataName: Array<String>
-    private lateinit var dataDescription: Array<String>
-    private lateinit var dataPhoto: TypedArray
-    private var heroes = arrayListOf<Hero>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
